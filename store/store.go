554dcd3f5ac15f144b93f0a9480bae2fbc2135f1
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-04-16 04:12:13 +0530

    Update

--- store/store.go
+++ store/store.go
@@ -15,45 +15,19 @@ type store struct {
 
 type Store interface {
 
-	// CreateUser creates a new user in the database.
-	// It takes a pointer to a User struct as input and returns an error if any occurs.
-	// The function uses a Cassandra query to insert the user details into the database.
-	// The user ID is generated using gocql.TimeUUID() and the created_at and updated_at timestamps are set to the current time.
-	// The function also sets the default status of the user to "active".
-	// If the user already exists, it returns an error.
+	//User store
+
 	CreateUser(user *types.User) error
-
-	// GetUserByID retrieves a user by their ID.
-	// It returns the user if found, or an error if any occurs.
-	// If the user is not found, it returns nil.
-	// If the user is found but marked as deleted, it returns nil.
-	// The function uses a Cassandra query to fetch the user details based on the ID.
-	// The ID is expected to be in the format of a UUID.
 	GetUserByID(id string) (*types.User, error)
-
-	// GetUserByEmail retrieves a user by their email address.
-	// It returns the user if found, or an error if any occurs.
-	// If the user is not found, it returns nil.
-	// If the user is found but marked as deleted, it returns nil.
-	// The function uses a Cassandra query to fetch the user details based on the email address.
 	GetUserByEmail(email string) (*types.User, error)
-
-	// SearchUsers retrieves a list of users based on the provided filter criteria.
-	// It returns a slice of pointers to User structs and an error if any occurs.
-	// The filter criteria can include name, email, mobile number, and status.
 	SearchUsers(filter types.UserFilter) ([]*types.User, error)
-
-	// Update Users with the given params
-	// Returns error if the database update failed
 	UpdateUser(user *types.User) error
-
 	DeleteUser(user *types.User) error
+
+
+	//Password Store
 }
 
-// NewStore initializes a new store instance with a Cassandra session.
-// It reads the Cassandra host from the environment variable "CASSANDRA_HOST".
-// If the session cannot be created, it panics.
-// The function returns a pointer to the store instance.
 func NewStore() Store {
 	session, err := gocql.NewCluster(os.Getenv("CASSANDRA_HOST")).CreateSession()
 	if err != nil {
