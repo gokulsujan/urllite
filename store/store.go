4e2e30d5648e8f4b37d3071244154467ea39956f
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-04-16 04:45:00 +0530

    Store logic addes for password

--- store/store.go
+++ store/store.go
@@ -24,12 +24,16 @@ type Store interface {
 	UpdateUser(user *types.User) error
 	DeleteUser(user *types.User) error
 
-
 	//Password Store
+	CreatePassword(password *types.Password) error
+	GetPasswordByUserID(userID string) (*types.Password, error)
+	UpdatePassword(passwrod *types.Password) error
 }
 
+var CASSANDRA_HOST, CASSANDRA_KEYSPACE = os.Getenv("CASSANDRA_HOST"), os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
+
 func NewStore() Store {
-	session, err := gocql.NewCluster(os.Getenv("CASSANDRA_HOST")).CreateSession()
+	session, err := gocql.NewCluster(CASSANDRA_HOST).CreateSession()
 	if err != nil {
 		panic(err)
 	}
@@ -38,8 +42,7 @@ func NewStore() Store {
 
 func (s *store) CreateUser(user *types.User) error {
 	// Implement user creation logic
-	keyspace := os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
-	createUserQuery := `INSERT INTO ` + keyspace + `.users (id, name, email, mobile, status, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)`
+	createUserQuery := `INSERT INTO ` + CASSANDRA_KEYSPACE + `.users (id, name, email, mobile, status, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)`
 	user.CreatedAt, user.UpdatedAt, user.ID = time.Now(), time.Now(), gocql.TimeUUID() // Generate a new UUID for the user adn set timestamps
 	user.Status = "active"                                                             // Set default status to active
 
@@ -48,8 +51,7 @@ func (s *store) CreateUser(user *types.User) error {
 
 func (s *store) GetUserByID(id string) (*types.User, error) {
 	var user types.User
-	keyspace := os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
-	getUserQueryById := "SELECT  id, name, email, mobile, status, created_at, updated_at, deleted_at FROM " + keyspace + ".users where id = ?"
+	getUserQueryById := "SELECT  id, name, email, mobile, status, created_at, updated_at, deleted_at FROM " + CASSANDRA_KEYSPACE + ".users where id = ?"
 	if err := s.DBSession.Query(getUserQueryById, id).Consistency(gocql.One).Scan(&user.ID, &user.Name, &user.Email, &user.Mobile, &user.Status, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt); err != nil {
 		return nil, err
 	}
@@ -67,8 +69,7 @@ func (s *store) GetUserByID(id string) (*types.User, error) {
 
 func (s *store) GetUserByEmail(email string) (*types.User, error) {
 	var user types.User
-	keyspace := os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
-	getUserQuery := `SELECT id, name, email, mobile, status, created_at, updated_at, deleted_at FROM ` + keyspace + `.users WHERE email = ? ALLOW FILTERING`
+	getUserQuery := `SELECT id, name, email, mobile, status, created_at, updated_at, deleted_at FROM ` + CASSANDRA_KEYSPACE + `.users WHERE email = ? ALLOW FILTERING`
 	if err := s.DBSession.Query(getUserQuery, email).Consistency(gocql.One).Scan(&user.ID, &user.Name, &user.Email, &user.Mobile, &user.Status, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt); err != nil {
 		return nil, err
 	}
@@ -83,8 +84,7 @@ func (s *store) GetUserByEmail(email string) (*types.User, error) {
 
 func (s *store) SearchUsers(filter types.UserFilter) ([]*types.User, error) {
 	var users []*types.User
-	keyspace := os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
-	searchUsersQuery := `SELECT id, name, email, mobile, status, created_at, updated_at FROM ` + keyspace + `.users`
+	searchUsersQuery := `SELECT id, name, email, mobile, status, created_at, updated_at FROM ` + CASSANDRA_KEYSPACE + `.users`
 	if filter.Name != "" || filter.Email != "" || filter.Mobile != "" || filter.Status != "" {
 		searchUsersQuery += ` WHERE`
 	}
@@ -173,8 +173,7 @@ func (s *store) UpdateUser(user *types.User) error {
 		return fmt.Errorf("No user id found")
 	}
 
-	keyspace := os.Getenv("CASSANDRA_URLLITE_KEYSPACE")
-	userUpdateQuery := "UPDATE " + keyspace + ".users SET name = ?, email = ?, mobile = ?, verified_email = ?, status = ?, updated_at = ? WHERE id = ?"
+	userUpdateQuery := "UPDATE " + CASSANDRA_KEYSPACE + ".users SET name = ?, email = ?, mobile = ?, verified_email = ?, status = ?, updated_at = ? WHERE id = ?"
 	return s.DBSession.Query(userUpdateQuery, user.Name, user.Email, user.Mobile, user.VerifiedEmail, user.Status, time.Now(), user.ID).Exec()
 }
 
@@ -187,4 +186,32 @@ func (s *store) DeleteUser(user *types.User) error {
 	userDeleteQuery := "UPDATE " + keyspace + ".users SET deleted_at = ? WHERE id = ?"
 
 	return s.DBSession.Query(userDeleteQuery, time.Now(), user.ID.String()).Exec()
+}
+
+func (s *store) CreatePassword(password *types.Password) error {
+	_, err := s.GetPasswordByUserID(password.UserID.String())
+	if err != gocql.ErrNotFound {
+		return fmt.Errorf("Password already setted for the user_id: ", password.UserID)
+	} else if err != nil {
+		return err
+	}
+	
+	createPasswordQuery := "INSERT INTO " + CASSANDRA_KEYSPACE + ".passwords (id, user_id, hashed_password, status, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)"
+	password.ID, password.CreatedAt, password.UpdatedAt = gocql.TimeUUID(), time.Now(), time.Now()
+	return s.DBSession.Query(createPasswordQuery, password.ID, password.UserID, password.HashedPassword, password.Status, password.CreatedAt, password.UpdatedAt).Exec()
+}
+
+func (s *store) GetPasswordByUserID(userID string) (*types.Password, error) {
+	var password types.Password
+	searchPasswordByUserIdQuery := "SELECT id, user_id, hashed_password, status, created_at, updated_at, deleted_at FROM " + CASSANDRA_KEYSPACE + ".passwords WHERE user_id = ? ALLOW FILTERING"
+	if err := s.DBSession.Query(searchPasswordByUserIdQuery, userID).Consistency(gocql.One).Scan(password.ID, password.UserID, password.HashedPassword, password.Status, password.CreatedAt, password.UpdatedAt, password.DeletedAt); err != nil {
+		return nil, err
+	}
+
+	return &password, nil
+}
+
+func (s *store) UpdatePassword(passwrod *types.Password) error {
+	updatePasswordQuery := "UPDATE " + CASSANDRA_KEYSPACE + ".passwords SET hashed_password = ?, updated_at = ? WHERE user_id = ? ALLOW FILTERING"
+	return s.DBSession.Query(updatePasswordQuery, passwrod.HashedPassword, time.Now(), passwrod.UserID).Exec()
 }
