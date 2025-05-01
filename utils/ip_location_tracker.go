df4ade0ce6da6c145497f177312471935db6adb6
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-05-01 12:58:25 +0530

    Use ipinfo api for ip location

--- utils/ip_location_tracker.go
+++ utils/ip_location_tracker.go
@@ -1,22 +1,35 @@
 package utils
 
 import (
-	"net"
-
-	"github.com/oschwald/geoip2-golang"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
 
-func GetIPAddressLocation(ipStr string) (map[string]string, error) {
-	db, err := geoip2.Open("GeoLite2-City.mmdb")
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
+type IPInfo struct {
+	IP       string `json:"ip"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Timezone string `json:"timezone"`
+}
 
-	ip := net.ParseIP(ipStr)
-	record, err := db.City(ip)
+func GetIPAddressLocation(ipStr string) (map[string]string, error) {
+
+	url := "https://ipinfo.io/" + ipStr + "/json"
+
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	return map[string]string{"country": record.Country.Names["en"], "city": record.City.Names["en"]}, nil
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var data IPInfo
+	json.Unmarshal(body, &data)
+
+	return map[string]string{"country": data.Country, "city": data.City}, nil
 }
